ef6561ac0b93f447f71761c810eedd742b4e06f3
shuanglengyunji <liuhan211211@gmail.com>
2017-06-18 20:15:57 +0800

    准备删除显示屏代码

--- User/include.h
+++ User/include.h
@@ -24,7 +24,7 @@
 //======== 图像传输 =================
 
 //#define __USART_DISPLAY
-#define __NRF_DISPLAY
+//#define __NRF_DISPLAY
 
 //显示内容（只能选择一个）：
 #define __DISPLAY_IMAGE	//显示图像
@@ -38,7 +38,7 @@
 
 //======== SD卡存图 ==================
 
-//#define __SD_SAVE
+#define __SD_SAVE
 
 //*************************************
 
